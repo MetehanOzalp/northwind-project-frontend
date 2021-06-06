a495cdda9d115fd7b69a38e06dc789695b76b478
Metehan Özalp <metehanozalp@hotmail.com>
2021-06-06 16:47:04 +0300

    products listed

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ProductList from "../pages/ProductList";
 import Categories from "./Categories";
-import Navi from "./Navi";
+import { Grid, GridColumn, GridRow } from "semantic-ui-react";
 
 export default function Dashboard() {
   return (
     <div>
-      <Navi />
-      <Categories />
-      <ProductList />
+      <Grid>
+        <GridRow>
+          <GridColumn width={4}>
+            <Categories />
+          </GridColumn>
+          <GridColumn width={12}>
+            <ProductList />
+          </GridColumn>
+        </GridRow>
+      </Grid>
     </div>
   );
 }
