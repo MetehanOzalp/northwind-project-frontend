e1ab08525fe8552ec8f27e2abf964d49b1d5480c
Metehan Özalp <metehanozalp@hotmail.com>
2021-06-10 18:28:34 +0300

    fake sign in and sign out operations added

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { Button, Container, Menu } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Container, Menu } from "semantic-ui-react";
 import CartSummary from "./CartSummary";
 import SignedIn from "./SignedIn";
 import SignedOut from "./SignedOut";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  function handleSignOut(params) {
+    setIsAuthenticated(false);
+  }
+
+  function handleSignIn(params) {
+    setIsAuthenticated(true);
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -14,8 +24,11 @@ export default function Navi() {
 
           <Menu.Menu position="right">
             <CartSummary />
-            <SignedOut />
-            <SignedIn />
+            {isAuthenticated ? (
+              <SignedIn signOut={handleSignOut} />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
           </Menu.Menu>
         </Container>
       </Menu>
