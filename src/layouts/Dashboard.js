83c7a57f1a6c60e3f1f7a2aaad8273625e5356c2
Metehan Özalp <metehanozalp@hotmail.com>
2021-06-10 18:47:20 +0300

    router operations added

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import ProductList from "../pages/ProductList";
 import Categories from "./Categories";
 import { Grid, GridColumn, GridRow } from "semantic-ui-react";
+import { Route } from "react-router";
+import ProductDetail from "../pages/ProductDetail";
 
 export default function Dashboard() {
   return (
@@ -12,7 +14,9 @@ export default function Dashboard() {
             <Categories />
           </GridColumn>
           <GridColumn width={12}>
-            <ProductList />
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/products" component={ProductList} />
+            <Route path="/products/:id" component={ProductDetail} />
           </GridColumn>
         </GridRow>
       </Grid>
